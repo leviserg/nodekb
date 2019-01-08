a163d454b73df138110a3c24c482f0b673fe4944
leviserg <sergey.levitskiy.sl@gmail.com>
2019-01-08 18:02:56 +0200

    link db

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
 module.exports = {
-    database: 'mongodb://localhost:27017/nodekb',
+    //database: 'mongodb://localhost:27017/nodekb',
+    database: 'mongodb://leviserg:aa111274@ds251894.mlab.com:51894/nodekb',
     secret: 'mysecret'
 }
